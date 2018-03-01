c5ebf2eb74948fc00423741b12f136230856a584
bsubedi26 <bsubedi26@gmail.com>
2018-02-28 19:05:03 -0500

    remove seeder func

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,15 +1,10 @@
 import { Request, Response } from "express";
-import User from "../models/User";
-import Topic from "../models/Topic";
-import Thread from "../models/Thread";
-import { seeder } from "./helpers";
 
 /**
  * GET /
  * Home page.
  */
 export let index = (req: Request, res: Response) => {
-  seeder(User, Topic, Thread);
   res.render("home", {
     title: "Home"
   });
